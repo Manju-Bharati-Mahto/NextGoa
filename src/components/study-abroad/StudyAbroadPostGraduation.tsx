3b70104e7b9803f5b6f7960605a5d96d3539b30f
Rohit Kumar <rrajput1172@gmail.com>
2026-06-03 11:14:03 +0530

    feat: add FAQ and Study Abroad components for landing and study-abroad pages

--- src/components/study-abroad/StudyAbroadPostGraduation.tsx
+++ src/components/study-abroad/StudyAbroadPostGraduation.tsx
@@ -48,7 +48,7 @@ export default function StudyAbroadPostGraduation() {
 
   return (
     <section 
-      className={`bg-[#f8f8f8] py-24 px-4 md:px-8 ${poppins.className}`}
+      className={`bg-[#f8f8f8] sm:py-24 pb-25 pt-5 px-4 md:px-8 ${poppins.className}`}
       style={poppins.style}
     >
       <div className="max-w-[1200px] mx-auto flex flex-col items-center">
