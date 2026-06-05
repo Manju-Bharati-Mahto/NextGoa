2cdd4cc7568fb8d2401f220316ab4226ab7cf166
Rohit Kumar <rrajput1172@gmail.com>
2026-06-06 05:10:31 +0530

    Update: increase top padding for mobile menu links

--- src/components/layout/SiteHeader.tsx
+++ src/components/layout/SiteHeader.tsx
@@ -117,7 +117,7 @@ export function SiteHeader() {
         <nav
           id="mobile-nav"
           aria-label="Mobile"
-          className="flex flex-col px-4 pt-4"
+          className="flex flex-col px-6 pt-12"
         >
           <ul className="flex flex-col gap-8">
             {navItems.map((item, idx) => (
