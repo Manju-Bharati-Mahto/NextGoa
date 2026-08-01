14eb06a9908cadff708450ccb7bc639bc0cee688
Rohit Kumar <rrajput1172@gmail.com>
2026-08-01 16:25:53 +0530

    feat: add schema markups for Contact, About Us and Blog pages

--- src/app/(website)/[slug]/page.tsx
+++ src/app/(website)/[slug]/page.tsx
@@ -52,6 +52,34 @@ export async function generateMetadata({
 
 export default async function DynamicPage({ params }: Props) {
   const { slug } = await params;
+  const response = await getPage(slug);
+  const page = response?.page;
 
-  return <PageRenderer slug={slug} />;
+  const breadcrumbSchema = page ? {
+    "@context": "https://schema.org/", 
+    "@type": "BreadcrumbList", 
+    "itemListElement": [{
+      "@type": "ListItem", 
+      "position": 1, 
+      "name": "Home",
+      "item": "https://goa.paruluniversity.ac.in/"  
+    },{
+      "@type": "ListItem", 
+      "position": 2, 
+      "name": page.title,
+      "item": `https://goa.paruluniversity.ac.in/${slug}`
+    }]
+  } : null;
+
+  return (
+    <>
+      {breadcrumbSchema && (
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbSchema) }}
+        />
+      )}
+      <PageRenderer slug={slug} />
+    </>
+  );
 }
